ffd753b1658d2d0cefbc90d5826b4031cb450ca2
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-08 18:13:31 +0530

    feat: add useMemo method to optimize searches and renders. added a search feature which searches on clicking the search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback } from "react";
+import { useState, memo, useCallback, useRef, useMemo } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 
@@ -27,26 +27,47 @@ function App(){
   ]);
 
   const [text, setText]=useState('');
+  const [search, setSearch]=useState('');
+  const inputRef=useRef<HTMLInputElement>(null);
 
   const handleChange=(e: React.ChangeEvent<HTMLInputElement>)=>{
     setText(e.target.value);
   }
 
+  const handleSubmit=()=>{
+    if(!inputRef.current) return;
+    setSearch(inputRef.current.value);
+  }
+
+  
+
   function addUsers(){
     setUsers([...users, {id: uuidv4(), name: text}]);
     setText('');
+    setSearch('');
+    if(!inputRef.current) return;
+    inputRef.current.value='';
   }
+
+  const filteredUsers = useMemo(()=>
+    users.filter(user=>user.name.toLowerCase()
+    .startsWith(search.toLowerCase())),
+    [search, users]);
 
   const removeUser=useCallback((item: User)=>{
     setUsers(prevUsers=>prevUsers.filter((user)=>user.id!==item.id));
   }, []);
 
+
   return(
     <div className="list-container">
       <h1>User List</h1>
-    <List users={users} removeUser={removeUser} />
-    <input className="user-input" type="text" onChange={handleChange} value={text} />
-    <button className="add-btn" onClick={addUsers}>Add</button>
+      <label>Search: </label>
+      <input ref={inputRef} />
+      <button type="submit" onClick={handleSubmit} >Search</button><br/>
+      <List users={filteredUsers} removeUser={removeUser} />
+      <input className="user-input" type="text" onChange={handleChange} value={text} />
+      <button className="add-btn" onClick={addUsers}>Add</button>
     </div>
   );
 
@@ -71,6 +92,7 @@ const List=memo(({users, removeUser}: ListProps)=>{
     </ul>
   );
 });
+
 type ListItemProps = {
   item: User,
   removeUser: (user: User)=>void
@@ -78,7 +100,7 @@ type ListItemProps = {
 
 const ListItem=memo(({item, removeUser}: ListItemProps)=>{
 
-  console.log("Render: ListItem");
+  console.log("Render: ListItem - "+ item.name);
   return(
     <li className="user-item">{item.name}
     <button className="remove-btn" type="button" onClick={()=>removeUser(item)}>Remove</button>
